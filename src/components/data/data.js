ce424b2147c98d90ab84cca93653858423d581c2
Sekou Abdoulaye Touré <satoure94@gmail.com>
2020-07-14 17:21:55 +0000

    :construction: work in progress.

---
+++ src/components/data/data.js
@@ -0,0 +1,27 @@
+export const kfcData = [
+  {
+    category: 'CHICKEN PARTS',
+    price: '4.50 USD',
+    description: '1 fry/cobette/salad - 1 drink 40cl',
+  },
+  {
+    category: 'HOT WINGS',
+    price: '2.50 USD',
+    description: '1 fry/cobette/salad - 1 drink 40cl',
+  },
+  {
+    category: 'TENDERS',
+    price: '3.50 USD',
+    description: '1 fry/cobette/salad - 1 drink 40cl',
+  },
+  {
+    category: 'BUCKET TENDERS',
+    price: '5.00 USD',
+    description: '2 frites/cobettes/ salades + 2 boissons 40cl',
+  },
+  {
+    category: 'BUCKET HOT WINGS',
+    price: '5.50 USD',
+    description: '1 fry/cobette/salad - 1 drink 40cl',
+  },
+];
